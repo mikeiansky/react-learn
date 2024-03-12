d4385f90ec1b4a56ef43404bca9eed76af4e5bdf
mikeian <zhouwenxiang@ciwei.net>
2024-03-12 18:46:18 +0800

    generator and normal test

--- src/pages/base/Generator.js
+++ src/pages/base/Generator.js
@@ -34,11 +34,58 @@ function testHelloGenerator() {
 
 }
 
+function* returnBeforeYield(){
+    yield 1
+    yield 4
+    yield 6
+    return 9
+    yield 8
+}
+
+function testReturnBeforeYield() {
+    const result = [...returnBeforeYield()]
+    console.log('return before yield result', result)
+}
+
+function * gen1() {
+    yield 11
+    yield 22
+    yield 33
+    yield 44
+}
+
+function * gen2() {
+    yield 111
+    yield 222
+    yield 333
+    yield 444
+    yield 555
+    return 666
+}
+
+function * genColl(c1, c2) {
+    yield * c1
+    yield * c2
+}
+
+function testGenColl() {
+
+    const c1 = gen1()
+    const c2 = gen2()
+
+    for (let v of genColl(c1, c2)) {
+        console.log('test gen coll : ',v)
+    }
+
+}
+
 export default function App(){
 
 
     testHelloGenerator()
     testF()
+    testReturnBeforeYield()
+    testGenColl()
 
     return (
 
--- src/pages/base/Normal.js
+++ src/pages/base/Normal.js
@@ -1,20 +1,42 @@
-
-function testAttr(){
+function testAttr() {
 
     const age_attr = 'age'
 
     const obj = {
-        name : 'ciwei',
-        [age_attr] : 32
+        name: 'ciwei',
+        [age_attr]: 32
     }
 
     console.log(obj.name, obj.age, obj.age_attr, obj[age_attr])
 
 }
 
-export default function App(){
+
+function testFunction() {
+
+    let log = (function () {
+        console.log('temp function execute')
+        return 'temp'
+    }())
+
+    console.log('test function log value is ', log)
+
+    let rf = (function () {
+        console.log('rf execute ... ')
+        return function () {
+            console.log('rf return function execute ... ')
+            return 'change by re'
+        }
+    }())()
+
+    console.log('rf value is ', rf)
+
+}
+
+export default function App() {
 
     testAttr()
+    testFunction()
 
     return (
         <div>
