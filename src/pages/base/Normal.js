ec0f4f2efdc79e944d854ca845c0f7541824a43d
mikeian <zhouwenxiang@ciwei.net>
2024-03-14 17:34:40 +0800

    javascript基础用法测试

--- src/pages/base/Normal.js
+++ src/pages/base/Normal.js
@@ -33,14 +33,81 @@ function testFunction() {
 
 }
 
+function testLambdaReturn() {
+    console.log('testLambdaReturn start ...')
+    const world = 'hello'
+    const lf = ({tag}) => ({tag})
+
+    const v = lf({tag: world})
+
+    console.log('labmda v is ', v)
+    console.log('labmda v.tag is ', v.tag)
+    console.log('testLambdaReturn end ...')
+
+}
+
+function connect(func) {
+    console.log('connect start ...')
+    const fv = func({tag: 'good'})
+    console.log('connect fv value is ', fv)
+    console.log('connect complete ...')
+    return () => fv
+}
+
+function testConnect() {
+    console.log('testConnect start ...')
+
+    const cv = connect(({tag}) => ({tag}))()
+    console.log('testConnect cv is ', cv)
+
+    console.log('testConnect complete ...')
+}
+
+function twoObjArgFunc({one}, {two, three}) {
+    console.log('one', one)
+    console.log('two', two)
+    console.log('three', three)
+}
+
+function testObjArg() {
+    console.log('testObjArg start ...')
+
+    twoObjArgFunc({one: 'first'}, {two: 'second', three: 'thrid'})
+
+    console.log('testObjArg complete ...')
+}
+
+function PropsApp(props) {
+
+    console.log('props app value is', props)
+
+    return <div>
+        This is props app
+    </div>
+}
+
 export default function App() {
 
     testAttr()
     testFunction()
+    testLambdaReturn()
+    testConnect()
+    testObjArg()
+
+    const person = {
+        name: 'ciwei',
+        // msg: 'ciwei-00',
+        age : 45
+    }
 
     return (
         <div>
             Normal
+            <PropsApp
+                msg={'start'}
+                port={80}
+                {...person}
+            />
         </div>
     )
 
--- src/pages/react/ReactNormal.js
+++ src/pages/react/ReactNormal.js
@@ -1,6 +1,21 @@
 
 
 
+function Hello(props) {
+
+    console.log('hello props', props)
+
+    return (
+        <>
+            <h1>hello msg: {props.msg}</h1>
+            <div>
+                {props.children}
+            </div>
+        </>
+    )
+
+}
+
 
 export default function App() {
 
@@ -8,6 +23,11 @@ export default function App() {
     return (
         <div>
             <h1>React Normal Page</h1>
+            <Hello msg={'good'} age={28}>
+                hello start<br/>
+                <p>hello project</p>
+                hello end<br/>
+            </Hello>
         </div>
     )
 
