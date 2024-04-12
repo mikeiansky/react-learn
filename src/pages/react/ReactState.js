0337cc77f6c3c35196eec8f93d060bf48b335e1f
mikeian <zhouwenxiang@ciwei.net>
2024-04-12 15:48:14 +0800

    react 学习

--- src/pages/react/ReactState.js
+++ src/pages/react/ReactState.js
@@ -4,6 +4,7 @@ import {useState} from "react";
 export default function () {
 
     const [count, setCount] = useState(0)
+    const [count2, setCount2] = useState(0)
 
     return (
         <div>
@@ -14,6 +15,19 @@ export default function () {
             </button>
             <br/>
             state count value is : {count}
+            <br/>
+
+            <button onClick={()=>{
+                setCount2((count) => {
+                    return count + 1
+                })
+            }}>
+                add variety
+            </button>
+            <br/>
+            count2 : {count2}
+            <br/>
+
         </div>
     )
 
