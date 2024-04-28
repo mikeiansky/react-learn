0e40e15849671c91c4f7da7f78af160bd244a6d7
mikeian <zhouwenxiang@ciwei.net>
2024-04-28 14:52:30 +0800

    useEffect测试

--- src/pages/react/ReactNormal.js
+++ src/pages/react/ReactNormal.js
@@ -1,4 +1,4 @@
-
+import {useEffect, useState} from "react";
 
 
 function Hello(props) {
@@ -19,6 +19,18 @@ function Hello(props) {
 
 export default function App() {
 
+    const [memberList, setMemberList] = useState([1])
+    console.log('memberList', memberList)
+
+    useEffect(()=>{
+        let data = {
+            list : [11,22,33,44]
+        }
+        data.list = null
+        console.log('use effect running ... ')
+        setMemberList(data?.list || [])
+        console.log('use effect running member list', memberList)
+    },[])
 
     return (
         <div>
